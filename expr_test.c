e6fc71030f8a648195ea18a21bca9d3b509a9381
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-01-12 14:02:17 +0200

    added more benchmarks

--- expr_test.c
+++ expr_test.c
@@ -290,15 +290,14 @@ static void test_name_collision() {
   test_expr("next=2,next(5)+next", 8);
 }
 
-static void test_benchmark() {
+static void test_benchmark(const char *s) {
   struct timeval t;
   gettimeofday(&t, NULL);
   double start = t.tv_sec + t.tv_usec * 1e-6;
   struct expr_var_list vars = {0};
-  char *s = "x=2+3*(x/(42+next(x))),x";
   struct expr *e = expr_create(s, strlen(s), &vars, user_funcs);
   if (e == NULL) {
-    printf("FAIL: %s returned NULL\n", s);
+    printf("FAIL: %s can't be compiled\n", s);
     status = 1;
     return;
   }
@@ -310,7 +309,7 @@ static void test_benchmark() {
   double end = t.tv_sec + t.tv_usec * 1e-6;
   expr_destroy(e, &vars);
   double ns = 1000000000 * (end - start) / N;
-  printf("TIME: %f ns/op (%dM op/sec)\n", ns, (int)(1000 / ns));
+  printf("BENCH %40s:\t%f ns/op (%dM op/sec)\n", s, ns, (int)(1000 / ns));
 }
 
 static void test_bad_syntax() {
@@ -369,7 +368,19 @@ int main() {
 
   test_bad_syntax();
 
-  test_benchmark();
+  test_benchmark("5");
+  test_benchmark("5+5+5+5+5+5+5+5+5+5");
+  test_benchmark("5*5*5*5*5*5*5*5*5*5");
+  test_benchmark("5,5,5,5,5,5,5,5,5,5");
+  test_benchmark("((5+5)+(5+5))+((5+5)+(5+5))+(5+5)");
+  test_benchmark("x=5");
+  test_benchmark("x=5,x+x+x+x+x+x+x+x+x+x");
+  test_benchmark("x=5,((x+x)+(x+x))+((x+x)+(x+x))+(x+x)");
+  test_benchmark("a=1,b=2,c=3,d=4,e=5,f=6,g=7,h=8,i=9,j=10");
+  test_benchmark("a=1,a=2,a=3,a=4,a=5,a=6,a=7,a=8,a=9,a=10");
+  test_benchmark("$(sqr,$1*$1),5*5");
+  test_benchmark("$(sqr,$1*$1),sqr(5)");
+  test_benchmark("x=2+3*(x/(42+next(x))),x");
 
   return status;
 }
